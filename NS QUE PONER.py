5101273649fa3e1c06a9e9b34a9efb1cbfef33bb
RSSChenChu <shenchu.jin@edu.uah.es>
2023-03-16 15:16:23 +0100

    BROOOOOO...

--- NS QUE PONER.py
+++ NS QUE PONER.py
@@ -39,3 +39,34 @@ def actualizar_sprite_movil(sprite_movil):
         sprite_movil.center_y = arcade.random_integers(0, HEIGHT)
 
 
+def detectar_colisiones(sprite_controlado, sprite_puntuacion, sprite_game_over, puntos):
+    if arcade.check_for_collision(sprite_controlado, sprite_puntuacion):
+        sprite_puntuacion.kill()
+        puntos += 1
+    if arcade.check_for_collision(sprite_controlado, sprite_game_over):
+        arcade.close_window()
+        print("GAME OVER")
+    return puntos
+
+
+def dibujar_sprites(sprite_controlado, sprite_puntuacion, sprite_game_over):
+    sprite_controlado.draw()
+    sprite_puntuacion.draw()
+    sprite_game_over.draw()
+
+
+def main():
+    juego = Juego()
+    sprite_controlado = SpriteControlado("sprite_controlado.png", 0.5)
+    sprite_controlado.position = (WIDTH // 2, HEIGHT // 2)
+    sprite_puntuacion = SpritePuntuacion("sprite_puntuacion.png", 0.5, 100, 100)
+    sprite_game_over = SpriteGameOver("sprite_game_over.png", 0.5, WIDTH // 2, HEIGHT // 2)
+    puntos = 0
+
+    @juego.event
+    def on_draw():
+        arcade.start_render()
+        dibujar_sprites(sprite_controlado, sprite_puntuacion, sprite_game_over)
+
+    @juego.event
+    def on_mouse_motion(x, y, dx, dy
